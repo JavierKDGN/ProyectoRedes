5c52de4b2d3c8bc4990618b26a7fca536b077bbe
JavierKDGN <javier.cadagan6@gmail.com>
2025-06-30 02:16:08 -0400

    Actualizar la configuración de la base de datos para usar una ruta relativa

--- final_server/app/database.py
+++ final_server/app/database.py
@@ -1,11 +1,13 @@
 # Database setup usando SQLite de motor
 # SQLAlchemy mapea objetos de python a elementos en la base de datos
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
