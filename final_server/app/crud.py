b312d42ecdeeac40738375334f3cb962c6dbb076
JavierKDGN <javier.cadagan6@gmail.com>
2025-06-30 01:40:05 -0400

    Operaciones CRUD listas para sensor readings y rutas de API implementadas en main

--- final_server/app/crud.py
+++ final_server/app/crud.py
@@ -1,1 +1,23 @@
 # Database operations (Create, Read, Update, Delete)
+
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_readings(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.SensorReadings)\
+        .order_by(models.SensorReadings.timestamp.desc())\
+        .offset(skip)\
+        .limit(limit).all()
+
+def get_readings_by_sensor_id(db: Session, sensor_id: int):
+    return db.query(models.SensorReadings)\
+        .filter(models.SensorReadings.sensor_id == sensor_id)\
+        .order_by(models.SensorReadings.timestamp.desc())\
+        .all()
+
+def create_reading(db:Session, reading: schemas.SensorReadingCreate):
+    db_reading = models.SensorReadings(**reading.model_dump())
+    db.add(db_reading)
+    db.commit()
+    db.refresh(db_reading)
+    return db_reading
--- final_server/app/main.py
+++ final_server/app/main.py
@@ -1,13 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
 
+from . import crud, models, schemas
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/readings/", response_model=schemas.SensorReading)
+def create_reading(reading: schemas.SensorReadingCreate, db: Session = Depends(get_db)):
+    return crud.create_reading(db=db, reading=reading)
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/readings/", response_model=list[schemas.SensorReading])
+def read_readings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    readings = crud.get_readings(db, skip = skip, limit = limit)
+    return readings
